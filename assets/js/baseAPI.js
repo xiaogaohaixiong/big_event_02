d2e755f6ae2e12ef3ce2a99efad0180ab4e68c8c
gaohaixiong555 <gaohaixiong@itcast.cn>
2021-03-15 19:57:39 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,7 +7,30 @@ let baseURL = 'http://api-breakingnews-web.itheima.net'
 
 // 拦截所有ajax请求：get、post、ajax；
 // 处理参数：
-$.ajaxPrefilter(function(params){
-    // 拼接对应环境的服务器地址
-    params.url = baseURL + params.url;
+
+
+$.ajaxPrefilter(function (options) {
+    //1. 拼接对应环境的服务器地址
+    // console.log(options);
+
+    options.url = baseURL + options.url;
+
+    // 2。身份认证
+    if (options.url.indexOf('/my/') != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 3.拦截所有响应，判断身份认证信息
+    options.complete = function (res) {
+        console.log(res.responseJSON);
+        let obj = res.responseJSON;
+        if (obj.status == 1 && obj.message == '身份认证失败！') {
+            // 1。清空本地token
+            localStorage.removeItem('token');
+            // 2.页面跳珠
+            location.href = "/login.html";
+        }
+
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,60 @@
+// 入口函数
+$(function () {
+    // 1.获取用于信息
+    getUserInof();
+    // 退出
+    let layer = layui.layer;
+    $('#btnLogout').on('click', function () {
+        // 框架提供的询问框
+        layer.confirm('是否确认退出?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // 1.清空本地token
+            localStorage.removeItem('token');
+            // 2.页面跳转
+            location.href = '/login.html'
+            // 3.关闭询问框
+            layer.close(index);
+        });
+    })
+});
+
+// 获取用于信息（封装到入口函数的外面了）
+// 原因：后面其他的页面要调用。
+function getUserInof() {
+    // 发送Ajax
+    $.ajax({
+        type: 'get',
+        url: '/my/userinfo',
+        data: {},
+        // headers: {
+        //     // 重新登录，因为token过期事件12小时
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: (res) => {
+            // console.log(res);
+            if (res.status != 0) {
+                return layui.layer.msg(res.message, { icon: 5 })
+            }
+            // 请求成功，渲染头像
+            renderAvatar(res.data);
+        }
+    })
+};
+
+function renderAvatar(user) {
+    // 1.渲染名称（nickname优先，如果没有，就用username）
+    let name = user.nickname || user.username;
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    // 2.渲染头像
+    if (user.user_pic != null) {
+        // 有头像
+        $('.layui-nav-img').show().attr('src', user.user_pic);
+        $('.text-avatar').hide();
+    } else {
+        // 没有头像
+        $('.layui-nav-img').hide();
+        $('.text-avatar').show().html(name[0].toUpperCase())
+    }
+}
+
+
